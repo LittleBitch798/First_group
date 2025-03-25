1d6cdf6ebb40ea9c545a1179b6f857a15d4f865c
沐辰iCloud <3123215356@qq.com>
2025-03-26 00:31:25 +0800

    WXH_commit326

---
+++ First_group/src/Collaborators/WXH/comText.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from "react";
+
+
+
+const comText = () =>{
+
+
+
+    return(
+        <>
+            <view>提交测试1</view>
+        </>
+    )   
+
+
+}
+
+export default comText;
